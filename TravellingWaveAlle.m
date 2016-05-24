118f637cbb5506a4f0b5fb86dc962623ff63b109
YasminaAdimy <yasmina.adimy@insa-lyon.fr>
2016-05-24 12:32:48 +0200

    Model Allee travelling wave en 1D
    Creation du fichier, modification de l'équation, création d'une fonction Allee. Problème : Comment définit on la condition initiale ?

---
+++ TravellingWaveAlle.m
@@ -0,0 +1,116 @@
+
+function sol=TravellingWaveAlle()
+% Solver for the Fisher/KPP equation with homogeneous Neumann boundary conditions
+% in 1D using finite differences
+% 
+% u_t - \Delta u = ku(1-u)(u-A)
+%
+
+
+close all;
+clear all;
+
+d=0.5;
+alpha=1;
+K=0.03;
+A=3;
+k=0.1;
+
+a = -50;
+b = 50;
+nel = 200; % number of elements
+h = (b-a)/nel; % step size
+nv = nel+1;% number of vertices
+
+x = a:h:b; % mesh
+dt = 0.01; % time steps
+tend = 50; 
+
+e=ones(nv, 1);
+
+% Discretization of the Laplace operator with Neumann bc
+Delta=1/(h^2)*spdiags([e -2*e e], -1:1, nv, nv);
+Delta(1,2) = 2/h^2;
+Delta(end,end-1) = 2/h^2;
+
+% initial guess
+u = K * (x < -20 ) ; %comprend pas si il faut changer ca !!
+ustore(1,:) = u;
+counter=1;
+figure(1)
+hold on;
+
+    % fonction KPP
+    function y=Allee(u)
+        y=k*u.*(1-u).*(u-A);
+    end    
+
+% explicit Euler scheme
+for t=dt:dt:tend
+    
+    u = u + dt * (d*(Delta * u')' +  Allee(u));
+ 
+    counter=counter+1;
+    ustore(counter,:) = u;   
+    
+    if(mod(counter,1000) == 0)
+        figure(2)
+        plot(x,u);
+        hold on
+    end
+end
+
+figure(1);
+plot(linspace(0,1,50), Allee(linspace(0,1,50)),'g',[0 1], [0 0],'--');
+title('f(u)')
+xlabel('u')
+ylabel('f(u)')
+
+figure(2)
+plot(x,u);
+title('Fronts d ondes pour différents temps');%tous les milles pas de temps
+xlabel('t')
+ylabel('u')
+
+figure(3);
+indX=[1,50,100,120,170,201];
+for i=1:1:6
+  subplot(2,3,i) 
+  plot(dt:dt:tend,ustore(2:length(ustore(:,indX(i))),indX(i)))
+  title(strcat('u(t), x =', num2str(indX(i)/2-50)))
+  xlabel('t')
+  ylabel('u')
+  axis([0 tend+1 0 K+0.01]);
+end
+
+
+figure(4)
+size(x)
+size(dt:dt:tend)
+size(ustore)
+surf(x,dt:dt:tend,ustore(2:length(ustore(:,1)),:),'edgecolor','none');
+
+xlabel('Distance x')
+ylabel('Time t')
+zlabel('Specie u')
+
+%%Je ne sais pas ce que ça affiche ???
+% [xmesh, tmesh] = meshgrid(x,0:dt:tend);
+% zmesh = xmesh-2 * diag(0:dt:tend) * ones(size(xmesh));
+
+% figure(2)
+% contour(xmesh,tmesh, ustore);
+% hold on
+% plot([10 20 20 10],[5 10 5 5],'--');
+% text(0,3,'Ref triangle with slope 2');
+% xlabel('x');
+% ylabel('t');
+% title('Contour plot of u(x,t)')
+% hold off
+
+%figure(3)
+%waterfall(zmesh, tmesh, ustore)
+%zlim([0 1])
+%xlim([-120,50])
+%view (50,30)
+end
