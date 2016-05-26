2e6ac07b14916857d7f8e41005220c5e63e0c406
YasminaAdimy <yasmina.adimy@insa-lyon.fr>
2016-05-26 20:22:08 +0200

    Travelling wave pour modele compétition a deux population
    Permet tracer u(x) et v(x) à différents instants, on observe que la population v s'éteind et u prend le dessus pour ces paramètres

---
+++ Comp_TravelingWaves.m
@@ -0,0 +1,74 @@
+% This function finds traveling-wave solutions for the
+% Pred-Prey model. To use it correctly, one must change
+% the parameters in each function defined in this file.
+function ppTravelingWaves
+
+% Solver for a diffusive version of the Fitz-Hugh-Nagumo system in 1D using
+% finite differences
+% 
+% u_t - eps du \Delta u = 1/eps (u (1-u)(u-alpha)-v)
+% v_t = gamma u - beta v
+%
+
+close all;
+clear all;
+
+a = 0;
+b = 100;
+nel = 200; % number of elements
+h = (b-a)/nel; % step size
+nv = nel+1;% number of vertices
+
+x = a:h:b; % mesh
+dt = 0.001; % time steps
+tend = 100; 
+
+e=ones(nv, 1);
+%Discretization of the Laplace operator
+Delta=1/(h^2)*spdiags([e -2*e e], -1:1, nv, nv);
+Delta(1,2) = 2/h^2;
+Delta(end,end-1) = -2/h^2;
+
+% parameters of the model
+alpha1=0.6;
+alpha2=0.6;
+K1=0.2;
+K2=0.2;
+gamma1=0.5;
+gamma2=1.5;
+d1=1;
+d2=0.5;
+
+% initial guess
+u = 0.03 * (x < 3);
+v = 0.2 * (x < 3);
+
+counter = 0;
+
+% explicit Euler scheme
+for t=dt:dt:tend
+    
+    u = u + dt * d1* ((Delta * u')') +  dt*alpha1.*u.*(1-u/K1-gamma1.*v/K1);
+    v = v +  dt * d2* ((Delta * v')') + dt * alpha2.*v.*(1-v/K2-gamma2.*u/K2);
+    
+    if(mod(counter,10000) == 0)
+        figure(1)
+        plot(x,u);
+        hold on;
+        
+        figure(2);
+        plot(x,v);
+        hold on; 
+    end
+    
+    counter = counter +1 ;
+
+    
+end
+
+%figure(1)
+%plot(x,u,x,v,'r');
+legend('u','v')
+title('Solution of the Fitz-Hugh-Nagumo system')
+
+end
